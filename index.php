459cd47115583fa732fd100432290ffa2087d232
marcquilloy11 <84653511+marcquilloy@users.noreply.github.com>
2024-12-19 16:02:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page!</p>
+	<p>Created by Marc Rafael S. Quilloy & Seifer Aquino</p>
+	<p>12-ICT</p>
+</div>
+
+<?=template_footer()?>
